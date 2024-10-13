36b6fb18bc413167407edd45d7ecab1f504a5cde
Azamat31415 <230147@astanait.edu.kz>
2024-10-13 17:06:23 +0500

    Added alert() to submit buttons

--- javascript/Azamat.js
+++
@@ -1,6 +0,0 @@
-let firstName = prompt("Enter your first name:");
-let lastName = prompt("Enter your last name:");
-let age = prompt("Enter your date of birth:")
-console.log("First name:" + firstName + "\n" +
-    "Last Name:" + lastName + "\n" +
-    "Age:" + age);
