5bccac3cb8d01f15667ac8bbbedf45b63a7bd7d6
yersultan056 <230129@astanait.edu.kz>
2024-10-30 21:48:29 +0500

    BAIKEEEEEEEEEEEEEEEEEEEEEEEEEN

--- JS/yers_js/personal_account.js
+++ JS/yers_js/personal_account.js
@@ -53,6 +53,21 @@ document.addEventListener('DOMContentLoaded', function() {
         let salesAds = JSON.parse(localStorage.getItem('salesAds')) || [];
         salesAds = salesAds.filter(ad => ad.location !== location);
         localStorage.setItem('salesAds', JSON.stringify(salesAds));
+        
+        // Удаление объявления из leaseAds
+        let leaseAds = JSON.parse(localStorage.getItem('leaseAds')) || [];
+        leaseAds = leaseAds.filter(ad => ad.location !== location);
+        localStorage.setItem('leaseAds', JSON.stringify(leaseAds));
+    
+        // Удаление объявления из cartAds
+        let cartAds = JSON.parse(localStorage.getItem('cartAds')) || [];
+        cartAds = cartAds.filter(ad => ad.location !== location);
+        localStorage.setItem('cartAds', JSON.stringify(cartAds));
+    
+        // Удаление объявления из cartSales
+        let cartSales = JSON.parse(localStorage.getItem('cartSales')) || [];
+        cartSales = cartSales.filter(ad => ad.location !== location);
+        localStorage.setItem('cartSales', JSON.stringify(cartSales));
     
         // Обновляем отображение объявлений
         displayAds();
